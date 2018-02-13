428ec811763f9c50d0410fc1b4a532926564f25f
Jack Goza <jackgoza@gmail.com>
2018-02-12 21:26:38 -0600

    save bc computer behaving bad

--- Lab2/source/classification/build.sbt
+++ Lab2/source/classification/build.sbt
@@ -7,6 +7,6 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "1.6.1" % "provided",
   "org.apache.spark" %% "spark-streaming" % "1.6.1",
-  "org.apache.spark" %% "spark-mllib" % "1.6.1"
+  "org.apache.spark" %% "spark-mllib" % "2.2.1"
 )
     
--- Lab2/source/classification/src/main/scala/kMeansClustering.scala
+++ Lab2/source/classification/src/main/scala/kMeansClustering.scala
@@ -1,5 +1,7 @@
-import org.apache.log4j.{Logger, Level}
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.ml.classification.NaiveBayes
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.Vectors
 
@@ -7,9 +9,6 @@ import org.apache.spark.mllib.linalg.Vectors
 object kMeansClustering {
 
   def main(args: Array[String]): Unit = {
-
-    //System.setProperty("hadoop.home.dir","C:\\Users\\Manikanta\\Documents\\UMKC Subjects\\PB\\hadoopforspark");
-
     val sparkConf = new SparkConf().setAppName("SparkWordCount").setMaster("local[*]")
 
     val sc=new SparkContext(sparkConf)
@@ -18,28 +17,34 @@ object kMeansClustering {
     Logger.getLogger("org").setLevel(Level.OFF);
     Logger.getLogger("akka").setLevel(Level.OFF);
     // Load and parse the data
-    val data = sc.textFile("data/3D_spatial_network.txt")
-    val parsedData = data.map(s => Vectors.dense(s.split(',').map(_.toDouble))).cache()
+
+    val testImages = sc.wholeTextFiles(s"data/output/mainframes/*.jpg")
+    val testImagesArray = testImages.collect()
+
+    val Array(training, test) = testImages.randomSplit(Array(0.6, 0.4))
+
+    val model = NaiveBayes.train()
 
     //Look at how training data is!
-    parsedData.foreach(f=>println(f))
+    // testImagesArray.foreach(f=>println(f))
 
-    // Cluster the data into two classes using KMeans
-    val numClusters = 4
-    val numIterations = 20
-    val clusters = KMeans.train(parsedData, numClusters, numIterations)
 
-    // Evaluate clustering by computing Within Set Sum of Squared Errors
-    val WSSSE = clusters.computeCost(parsedData)
-    println("Within Set Sum of Squared Errors = " + WSSSE)
-
-    //Look at how the clusters are in training data by making predictions
-    println("Clustering on training data: ")
-    clusters.predict(parsedData).zip(parsedData).foreach(f=>println(f._2,f._1))
-
-    // Save and load model
-    clusters.save(sc, "data/KMeansModel")
-    val sameModel = KMeansModel.load(sc, "data/KMeansModel")
+//    // Cluster the data into two classes using KMeans
+//    val numClusters = 4
+//    val numIterations = 20
+//    val clusters = KMeans.train(parsedData, numClusters, numIterations)
+//
+//    // Evaluate clustering by computing Within Set Sum of Squared Errors
+//    val WSSSE = clusters.computeCost(parsedData)
+//    println("Within Set Sum of Squared Errors = " + WSSSE)
+//
+//    //Look at how the clusters are in training data by making predictions
+//    println("Clustering on training data: ")
+//    clusters.predict(parsedData).zip(parsedData).foreach(f=>println(f._2,f._1))
+//
+//    // Save and load model
+//    clusters.save(sc, "data/KMeansModel")
+//    val sameModel = KMeansModel.load(sc, "data/KMeansModel")
 
 
   }
