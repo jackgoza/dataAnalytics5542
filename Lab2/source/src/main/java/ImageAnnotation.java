e083872f96a2d15512c0c6fb9784d3433d915578
Jack Goza <jackgoza@gmail.com>
2018-02-12 19:38:19 -0600

    halfway through lab 2

---
+++ Lab2/source/src/main/java/ImageAnnotation.java
@@ -0,0 +1,53 @@
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.api.ClarifaiResponse;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.input.image.ClarifaiImage;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.colour.RGBColour;
+import org.openimaj.image.typography.hershey.HersheyFont;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class ImageAnnotation {
+//    public static void main(String[] args) throws IOException {
+//        final ClarifaiClient client = new ClarifaiBuilder("KKQIegBW9uOl_3vaMSzqq4QCfPNyNBvB7XNBz1vE", "xsY48eiDhhsFo5M7HE3F71ZYkB_tEQmemlWekTgG")
+//                .buildSync(); // or use .build() to get a Future<ClarifaiClient>
+//        client.getToken();
+//
+//        File file = new File("output/mainframes2");
+//        File[] files = file.listFiles();
+//        for (int i = 0; i < files.length; i++) {
+//            ClarifaiResponse response = client.getDefaultModels().generalModel().predict()
+//                    .withInputs(
+//                            ClarifaiInput.forImage(ClarifaiImage.of(files[i]))
+//                    )
+//                    .executeSync();
+//            List<ClarifaiOutput<Concept>> predictions = (List<ClarifaiOutput<Concept>>) response.get();
+//            MBFImage image = ImageUtilities.readMBF(files[i]);
+//            int x = image.getWidth();
+//            int y = image.getHeight();
+//
+//            System.out.println("*************" + files[i] + "***********");
+//            List<Concept> data = predictions.get(0).data();
+//            for (int j = 0; j < data.size(); j++) {
+//                System.out.println(data.get(j).name() + " - " + data.get(j).value());
+//                image.drawText(data.get(j).name(), (int) Math.floor(Math.random() * x), (int) Math.floor(Math.random() * y), HersheyFont.ASTROLOGY, 20, RGBColour.RED);
+//                String name = "output/annotatedframes2/annotated" + i + ".jpg";
+//                File outputFile = new File(name);
+//                try {
+//                    ImageUtilities.write(image, outputFile);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//        System.out.println("Tttththththtat's all, folks!");
+//        System.exit(0);
+//    }
+}
